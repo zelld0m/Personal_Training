643e46490d3f7ff76769bc65faa8cea0ee8b6012
Aizel Nunag <izelllim@gmail.com>
2017-02-07 10:28:07 +0800

    saving

--- Test/WebApplication1/WebApplication1/Properties/AssemblyInfo.cs
+++ Test/WebApplication1/WebApplication1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ba7a9175-f3b7-4ade-b956-22b6572c1cd1")]
+[assembly: Guid("f14e5927-8ad3-4090-85d3-7b7dbf15c92c")]
 
 // Version information for an assembly consists of the following four values:
 //
