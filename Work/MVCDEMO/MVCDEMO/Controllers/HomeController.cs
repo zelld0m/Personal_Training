162ed6038b79fcd6960f6326063a6fa3aca9de9c
Aizel Nunag <izelllim@gmail.com>
2017-02-08 10:43:20 +0800

    MVC DEMO

---
+++ Work/MVCDEMO/MVCDEMO/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDEMO.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Countries()
+        {
+
+            // THIS IS NOT THE BEST PRACTICE  
+            //it just shows that it is possible to use 
+            //this kind of coding
+            ViewBag.Countries = new List<string>()
+            {
+                "INDIA",
+                "US",
+                "CANADA"
+            };
+            return View();
+        }
+    }
+}
