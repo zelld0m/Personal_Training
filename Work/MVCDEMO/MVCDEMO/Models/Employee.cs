472f00541049d139f18b635601535de46619e06b
Aizel Nunag <izelllim@gmail.com>
2017-02-16 15:38:26 +0800

    ReadOnly not working, [UIHint] ,DatatypeEmailaddress,url,date format

--- Work/MVCDEMO/MVCDEMO/Models/Employee.cs
+++ Work/MVCDEMO/MVCDEMO/Models/Employee.cs
@@ -6,19 +6,28 @@ using System.Web;
 using System.ComponentModel.DataAnnotations;
 namespace MVCDEMO.Models
 {
+   // [MetadataType(typeof(EmployeeMetadata))]
     [Table("tblEmployee")]          // This type of class needs to be the same as the Database tablename
     public class Employee
     {
-
         public int EmployeeId { get; set; }       //  
-                    //required removed To unEdit Name in Edit
+                                                  //required removed To unEdit Name in Edit
         [DisplayAttribute(Name = "NAMETest")]  // USING DISPLAY ATTRIBUTE FROM MODEL
         public string Name { get; set; }
         [DisplayFormat(NullDisplayText = "Gender not Specified")]
         public string Gender { get; set; }
         [Required]
+        //[ReadOnly(true)]   //Not working maybe on metadataType it will work  if not working  remove set is the same on getter:setter
+        [DataType(DataType.Url)]
+        [UIHint("OpenInNewWindow")] // downside of this is to open a new window of all links
         public string City { get; set; }
-        [Required] 
+        [Required]/* [ScaffoldColumn(false)]*/   // NOT WORKING
         public int DepartmentId { get; set; }
+        //[DataType(DataType.EmailAddress)]       // this will open default email client  and open outlook
+        //public String EmailAddress { get; set; }
+
+        //[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}",ApplyFormatInEditMode =true)]    // fix format for dates
+        //public DateTime? HireDate { get; set; }
     }
+   
 }
