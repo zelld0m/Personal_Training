9fbd0807253d26cca40310d2770e648cdf753946
Aizel Nunag <izelllim@gmail.com>
2017-03-07 13:23:02 +0800

    .

--- ConsoleTestExams/ConsoleTestExams/Program.cs
+++ ConsoleTestExams/ConsoleTestExams/Program.cs
@@ -8,17 +8,32 @@ namespace ConsoleTestExams
 {
     class Program
     {
+        #region DelegateTraining1
 
+        //delegate int mydel(int aa);
+        //int myfunc(int a) { return a * a; }
+        //int myfunc2(int a) { return a + a; }
+        //static void Main(String[] args)
+        //{
+        //    Program p = new Program();
+        //    mydel d = p.myfunc;
+        //    Console.WriteLine(d(5));
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine(Palindrome("OLLO"));
+        //    d = p.myfunc2;
+        //    Console.WriteLine(d(5));
+        //    Console.ReadLine();
+        //}
 
-            Console.ReadLine();
-        }
+        #endregion
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(Palindrome("OLLO"));
+
+        //    Console.ReadLine();
+        //}
 
         public static string Palindrome(String word)
-        {
+       {
             string rev = "";
             for (int i = word.Length - 1; i >= 0; i--)
             {
@@ -27,44 +42,35 @@ namespace ConsoleTestExams
             rev = (rev == word ? "PALINDROME" : "NOT PALINDROME");
             return rev;
         }
-
-//        A company named ACME wants to modify one of its system named ACMEPlus to be able to
-//communicate with another existing system named Jewel.Jewel transmits data by XML, and
-//numerical values are represented in hexadecimal.
-//Unfortunately, there is a known bug in Jewel.Instead of following the standard hexadecimal
-//representation, Jewel represents some hexadecimal values using different characters.This
-//table shows the differences:
-//Decimal Standard Hexadecimal
-//Representation
-//Jewel Hexadecimal
-//Representation
-
-//0 0 0
-//1 1 1
-//2 2 2
-//3 3 3
-//4 4 4
-//5 5 5
-//6 6 6
-//7 7 7
-//8 8 8
-//9 9 9
-//10 A Q
-//11 B W
-//12 C E
-//13 D R
-//14 E T
-//15 F Y
-
-//For example, the standard hexadecimal representation of the the Jewel hexadecimal
-//“QWERTY” is “ABCDEF”. The decimal representation of the standard hexadecimal “ABCDEF”
-//is 11259375.
-//Using the sample code below, your job is to implement the method “parseJewelHex” using
-//Java that parses Jewel’s numeric data, into a Java integer.
- 
-       public static string parseJewelHex(string jewelHex)
+    }
+    interface interface1 { void showMe(); }
+    interface interface2 { void showYou(); }
+    class BaseClass
+    {
+        public virtual void show()
         {
-            return "";
+            System.Console.WriteLine("base class");
+        }
+    }
+    class DerivedAndImplemented : BaseClass, interface1, interface2
+    {
+        public void showMe()
+        {
+            System.Console.WriteLine("Me!");
+        }
+        public void showYou()
+        {
+            System.Console.WriteLine("You!");
+        }
+        public override void show()
+        {
+            Console.WriteLine("im in derived class");
+        }
+        static void main(String[] args)
+        {
+            DerivedAndImplemented de = new DerivedAndImplemented();
+            de.show();
+            System.Console.Read();
         }
     }
 }
