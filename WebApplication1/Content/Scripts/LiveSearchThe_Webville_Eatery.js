469d4193200caad4ee909ed2d7146af1700f6003
Aizel Nunag <izelllim@gmail.com>
2017-03-17 17:06:54 +0800

    Note . all working

--- WebApplication1/Content/Scripts/LiveSearchThe_Webville_Eatery.js
+++ WebApplication1/Content/Scripts/LiveSearchThe_Webville_Eatery.js
@@ -1,18 +1,69 @@
-﻿
+﻿//Remember always check Cache or clear cache if not working
+
 $(document).ready(function () {
     $('#thai').mouseenter(function () {
-        $('.menu_list1').slideDown(500);
+        $('.menu_list1').slideDown(300);
     });
     $('#thai').mouseleave(function () {
-        $('.menu_list1').slideUp(500);
+        $('.menu_list1').slideUp(300);
     });
 });
 
 $(function () {
     $('#Panini').mouseenter(function () {
-        $('.menu_list2').slideDown(500);
+        $('.menu_list2').slideDown(300);
     });
     $('#Panini').mouseleave(function () {
-        $('.menu_list2').slideUp(500);
+        $('.menu_list2').slideUp(300);
+    });
+});
+
+$(function () {
+    $('#Southwest').mouseenter(function () {
+        $('.menu_list3').slideDown(300);
+    });
+    $('#Southwest').mouseleave(function () {
+        $('.menu_list3').slideUp(300);
+    });
+});
+
+
+
+
+
+
+
+
+//<!-- Working -->        // this wont work because of ('data-panelid') it was a created attribute on htmlpage
+$(function () { 
+    $('.panel-button').on('click', function () {
+        var panelId = $(this).attr('data-panelid');
+
+        $('.menu_list' + panelId).slideDown(300);
+    });
+    $('.panel-button').mouseleave(function () {
+        var panelId = $(this).attr('data-panelid');
+
+        $('.menu_list' + panelId).slideUp(100);
+    });
+});
+
+
+$(document).ready(function () {
+    $("#thaiwan").on('click', function () {
+        $("#thaiwan").slideToggle(500);
+        $("#thaiwan").slideToggle(10);
+    });
+});
+
+$(document).ready(function () {
+    $(".test1").css("background-color", "green");
+    $(".test1").on("click", function () {
+        alert("xxx");
+    });
+    $(".test1").on("click", function () {
+        $(".test1").css("background-color", "red");
     })
 });
+
+//<!-- Working  -->
---
+++ WebApplication1/Content/Scripts/maps.js
@@ -0,0 +1,13 @@
+﻿$(document).ready (function(){
+    var map;
+    function initialize(){
+    var latlng = new google.maps.LatLng(45.519098,-122.672138);
+    var mapOpts = {
+        zoom: 13,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOpts);
+}
+initialize();
+});
