379da60862688af089bf4e7477ad3c3b891b212e
Aizel Nunag <aizel_04@yahoo.com>
2018-11-13 14:00:04 +0800

    aizel-20181113-part1

--- MvcWebAPI/MvcWebAPI/Controllers/ValuesController.cs
+++ MvcWebAPI/MvcWebAPI/Controllers/ValuesController.cs
@@ -7,27 +7,39 @@ using System.Web.Http;
 
 namespace MvcWebAPI.Controllers
 {
-    public class ValuesController : ApiController
+    public class Person
     {
+        public int ID { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+
+    } 
+    public class PersonController : ApiController
+    {
+
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<Person> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new Person[] {
+                new Person{ ID=0, First="Scott", Last ="Hanselam" },
+                new Person{ ID=0, First="Scott1", Last ="Hanselam1" },
+                new Person{ ID=0, First="Scott2", Last ="Hanselam2" },
+            };
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public Person Get(int id)
         {
-            return "value";
+            return null;
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Person value)
         {
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Person value)
         {
         }
 
